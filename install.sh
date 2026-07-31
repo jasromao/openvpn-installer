ba915aa49330cd01eb9c643d22b9ee3439515aff
jasromao <jasromao@gmail.com>
2026-07-31 18:04:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 set -e
+echo "======================================="
+echo "  INSTALADOR OPENVPN AUTOMÁTICO"
+echo "======================================="
+
+apt update
+apt install -y curl wget unzip
+
+echo
+echo "Instalação concluída."
+echo
+echo "Em seguida será descarregado e restaurado o backup."
