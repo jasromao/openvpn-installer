f13b3f648f96e7cd3d339f6cd28351320d013398
jasromao <jasromao@gmail.com>
2026-07-31 21:58:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,76 +2,48 @@
 set -e
 
 echo "=========================================="
-echo "        OPENVPN INSTALLER"
+echo "   OPENVPN INSTALLER"
 echo "=========================================="
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Execute com sudo."
+    echo "Execute como root:"
+    echo "sudo bash"
     exit 1
 fi
 
 export DEBIAN_FRONTEND=noninteractive
 
 echo
-echo "[1/6] Atualizar o sistema..."
+echo "[1/5] Atualizar o sistema..."
 apt-get update
 
 echo
-echo "[2/6] Instalar dependências..."
+echo "[2/5] Instalar dependências..."
 apt-get install -y \
-    openvpn \
-    easy-rsa \
-    iptables \
-    curl \
-    wget \
-    tar \
-    unzip
-
-echo
-echo "[3/6] Credenciais GitHub"
-
-read -p "Utilizador GitHub: " GITHUB_USER
-read -rsp "GitHub Token: " GITHUB_TOKEN
-echo
+openvpn \
+easy-rsa \
+iptables \
+curl \
+wget \
+tar \
+unzip
 
 cd /root
 
 echo
-echo "[4/6] Descarregar backup..."
-
-curl -fL \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -o backup-openvpn.tar.gz \
-  "https://raw.githubusercontent.com/${GITHUB_USER}/openvpn-backup/main/backup-openvpn.tar.gz"
-
-if [ ! -f backup-openvpn.tar.gz ]; then
-    echo
-    echo "Erro ao descarregar o backup."
-    exit 1
-fi
+echo "[3/5] Descarregar backup..."
+wget -O backup-openvpn.tar.gz \
+https://raw.githubusercontent.com/jasromao/openvpn-installer/main/backup-openvpn.tar.gz
 
 echo
-echo "[5/6] Descarregar script de restauro..."
-
-curl -fL \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -o restaurar-openvpn.sh \
-  "https://raw.githubusercontent.com/${GITHUB_USER}/openvpn-backup/main/restaurar-openvpn.sh"
-
-if [ ! -f restaurar-openvpn.sh ]; then
-    echo
-    echo "Erro ao descarregar o script de restauro."
-    exit 1
-fi
+echo "[4/5] Descarregar script de restauro..."
+wget -O restaurar-openvpn.sh \
+https://raw.githubusercontent.com/jasromao/openvpn-installer/main/restaurar-openvpn.sh
 
 chmod +x restaurar-openvpn.sh
 
-unset GITHUB_TOKEN
-unset GITHUB_USER
-
 echo
-echo "[6/6] Restaurar servidor..."
-
+echo "[5/5] Restaurar servidor..."
 bash restaurar-openvpn.sh
 
 echo
